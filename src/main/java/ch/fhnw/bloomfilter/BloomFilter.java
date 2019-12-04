538119db5e5df3ec1631a1ce140d27be851b53f0
severin0038 <severin.peyer@students.fhnw.ch>
2019-12-04 23:18:45 +0100

    Kommentare eingefügt

--- src/main/java/ch/fhnw/bloomfilter/BloomFilter.java
+++ src/main/java/ch/fhnw/bloomfilter/BloomFilter.java
@@ -19,12 +19,13 @@ public class BloomFilter {
     double p;
     double m;
     int k;
-
     boolean[] bloomFilter;
 
+    // Liste der einzufügenden Wörter erstellen
     Reader reader = new Reader("words.txt");
     ArrayList<String> words = reader.readFile();
 
+    // Liste der Testwerte erstellen
     Reader readertestValues = new Reader("testvalues.txt");
     ArrayList<String> testValues = readertestValues.readFile();
 
@@ -36,7 +37,7 @@ public class BloomFilter {
         this.k = (int)( (m / n) * Math.log(2)) + 1;                 // Anzahl Hash-Funktionen
         bloomFilter = new boolean[(int) m + 1];                     // Bloomfilter-Array
 
-
+        // alle Wörter der Wörterliste in den Bloomfilter speichern
         for(int i = 0; i < n; i++) { // Wörterliste
             for(int j = 0; j < k; j++) { // alle Hashfunktionen
                HashCode hash = Hashing.murmur3_128(j).hashString(words.get(i), Charset.defaultCharset());
@@ -46,6 +47,7 @@ public class BloomFilter {
         }
     }
 
+    //Prüfmethode, ob ein String in der Wortliste enthalten ist
     public boolean stringIsInList(String testString) {
         for(int j = 0; j < k; j++) { //alle Hashfunktionen
             HashCode hash = Hashing.murmur3_128(j).hashString(testString, Charset.defaultCharset());
@@ -56,6 +58,7 @@ public class BloomFilter {
         } return true;
     }
 
+    //Zuverlässigkeit anhand von einer grossen Menge an Testwerten überprüfen
     public double testReliability() {
         int numberOfWordsRightlyDetected = 0;
         int numberOfWordsWronglyDetected = 0;
@@ -63,6 +66,7 @@ public class BloomFilter {
         for(int i = 0; i < testValues.size(); i++) { // Liste mit Testwerten
 
             if(stringIsInList(testValues.get(i))) {
+                // Prüfen, ob der String tatsächlich in der Liste ist
                 if(words.contains(testValues.get(i))) {
                     numberOfWordsRightlyDetected++;
                 } else {
