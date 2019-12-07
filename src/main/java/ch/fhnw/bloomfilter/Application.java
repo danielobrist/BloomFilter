cc6cca3c7a81149318d8c7423a1eeaedbf31af41
danielobrist <daniel.obrist@students.fhnw.ch>
2019-12-07 12:22:37 +0100

    added option to input p values (only decimal values with . work for now)

--- src/main/java/ch/fhnw/bloomfilter/Application.java
+++ src/main/java/ch/fhnw/bloomfilter/Application.java
@@ -1,15 +1,20 @@
 package ch.fhnw.bloomfilter;
 
 import java.io.IOException;
-import java.net.URISyntaxException;
+import java.util.Locale;
+import java.util.Scanner;
 
 public class Application {
-    public static void main(String[] args) throws URISyntaxException, IOException {
+    public static void main(String[] args) throws IOException {
+        System.out.println("Bitte gewünschte Fehlerwahrscheinlichekeit p eingeben (z.B. 0.01 für 1%)");
+        System.out.println("-> Achtung es funktioniert nur Punkt als Dezimaltrennzeichen!");
+        Scanner in = new Scanner(System.in).useLocale(new Locale("de_CH"));
+        double p = in.nextDouble();
 
-        BloomFilter bloomFilter = new BloomFilter(0.01);
+        BloomFilter bloomFilter = new BloomFilter(p);
 
         System.out.println("Anzahl Wörter: " + bloomFilter.getN());
-        System.out.println("gewünschte maximale Fehlerwahrscheinlichkeit: " + (int) (bloomFilter.getP()*100) + "%");
+        System.out.println("gewünschte maximale Fehlerwahrscheinlichkeit: " + (bloomFilter.getP()*100) + "%");
         System.out.println("Anzahl Hash-Funktionen: " + bloomFilter.getK());
         System.out.println("Anzahl Testwerte: " + bloomFilter.getZ());
         System.out.println("Zuverlässigkeit: " + bloomFilter.testReliability());
